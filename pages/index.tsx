3c92543c23288caae3f846457af6b9d1266f64f0
Dominik <gradiskid@gmail.com>
2022-09-29 23:18:03 +0200

    added header with styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Dominik&apos;s portfolio</title>
       </Head>
+      {/* testing the initial tailwind config */}
+      {/* <h1 className='p-10 text-red-500'>Lets build  an AWESOME portfolio</h1> */}
 
-      <h1>Lets build  an AWESOME portfolio</h1>
+      {/* Header */}
+      <Header/>
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+
     </div>
   )
 }
