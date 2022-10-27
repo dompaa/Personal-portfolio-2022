31558f1274869c03b4b8914ccd36a633972e1664
Dominik <gradiskid@gmail.com>
2022-10-27 22:22:36 +0200

    included all components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import About from '../components/About'
+import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
 const Home: NextPage = () => {
@@ -34,10 +37,19 @@ const Home: NextPage = () => {
       </section>
 
       {/* Skills */}
-
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
       {/* Projects */}
 
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
+
       {/* Contact Me */}
+      <section>
+      <ContactMe/>
+      </section>
 
     </div>
   )
